0595ae12666213f3c96a2cde7cb50282df43c8f9
RinatGibadullin <Rinat990104@yandex.ru>
2020-05-26 13:42:18 +0300

    added hashing

--- links/urls.py
+++ links/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'links'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('new/', views.CreateView.as_view(), name='new'),
+    path('create/', views.create_tiny_link, name='create'),
 ]
--- links/views.py
+++ links/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+import hashlib
+import uuid
+
+import bcrypt as bcrypt
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.utils import timezone
+from django.views import generic
 
 from .models import Link
-from django.views import generic
 
 
 class IndexView(generic.ListView):
@@ -12,4 +14,25 @@ class IndexView(generic.ListView):
     context_object_name = 'links_list'
 
     def get_queryset(self):
-        return Link.objects.filter()[:5]
+        return Link.objects.filter()
+
+
+class CreateView(generic.CreateView):
+    template_name = 'links/new.html'
+    model = Link
+    fields = ['orig_link']
+
+
+def get_tiny_link(orig_link):
+    tiny_link = bcrypt.hashpw(orig_link.encode('utf-8'), bcrypt.gensalt())
+    return tiny_link.decode("utf-8")[30:40]
+
+
+def create_tiny_link(request):
+    orig_link = request.POST['orig_link']
+    link = Link.objects.create(
+        orig_link=orig_link,
+        tiny_link=get_tiny_link(orig_link=orig_link)
+    )
+    link.save()
+    return HttpResponseRedirect(reverse('links:index'))
